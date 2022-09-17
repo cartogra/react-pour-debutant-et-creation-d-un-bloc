0431f8998ab8733c3e2b0a4a6e894306cebdc66d
djougue <anniechristelledjougue@gmail.com>
2022-09-17 12:19:14 +0100

    interpolation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+const titre= 'bonjour le monde'
+const contenu= 'contenu de la page'
+const like= 150
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenu'>
+      
+     <h1>{titre}</h1>
+     <p>{contenu}</p>
+     <p> { [1,2,3,4] }</p>
+     <p> {Math.random() *10 }</p>
+    </div>
     </div>
   );
 }
