5edf5b17746fb6aacf1e29e84a27dae21a90d65f
djougue <anniechristelledjougue@gmail.com>
2022-09-20 13:32:48 +0100

    ajout de db

--- src/Home.js
+++ src/Home.js
@@ -1,41 +1,37 @@
- import { useState } from "react";
+ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+
+
 const Home = () => {
    
-const [blogs, setBlog] = useState([
+const [blogs, setBlog] = useState([]);
 
-  {
-    id:1,
-    title: "bienvenu a mon blog.",
-    body: "a dui. cras pellentesque. sed dictum. proin eget odio", 
-    author: "annie",
-    date: "19/09/2022"
-  },
-  {
-    id:2,
-    title: "j'apprend a coder",
-    body: "cursus vestibulum. mauris magna. duis dignissin temporq",
-    author: "christelle",
-    date: "19/09/2022"
-  },
+useEffect( () => {
+    fetch('http://localhost:5500/src/data/db.json')
+     .then  ((response) => {
 
-  {
-    id:3,
-    title: "je veux maitriser le code",
-    body: "cursus vestibulum. mauris magna. duis dignissin temporq",
-    author: "christelle",
-    date: "19/09/2022"
-  },
-]);
+        return response.json();
+    })
+    .then ((data) => {
+      console.log(data);    
+      console.log(data['blogs']);    
+       setBlog(data['blogs'])
+        
+   })
+
+ }, [])
+
+
 const handleDelete= (id) => {
     const newBlogs= blogs.filter( (blog) => blog.id !== id);
     setBlog(newBlogs);
 
+    
  }
     return ( 
         <div className="home">
-           <BlogList blogs={blogs} title={'liste des blogs'} handleDelete={handleDelete}/>
-           <BlogList blogs={blogs.filter ((blog)  =>blog.author === 'christelle')} title={'liste des blogs publier par christelle'}/>
+            {blogs && <BlogList blogs={blogs} title={'liste des blogs'} handleDelete={handleDelete}/> }
+             {/* <BlogList blogs={blogs.filter ((blog)  =>blog.author === 'christelle')} title={'liste des blogs publier par christelle'}/> */}
         </div>
      );
 }
